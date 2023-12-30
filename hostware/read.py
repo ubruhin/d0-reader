8f5336cd97906d325a11235867e159c06f8b4374
U. Bruhin <urbibruhin@bluewin.ch>
2023-12-30 21:33:47 +0100

    Hostware: Print actual power

--- hostware/read.py
+++ hostware/read.py
@@ -2,8 +2,17 @@
 
 import socket
 import sys
+import re
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(("192.168.1.99", 80))
+    s.connect(("192.168.1.51", 80))
+    current_data = ""
     while True:
-        sys.stdout.write(s.recv(100).decode('ascii', 'ignore'))
+        data = s.recv(100).decode('ascii', 'ignore')
+        sys.stdout.write(data)
+        current_data += data
+        if current_data.endswith('!\r\n'):
+            m = re.search("1\\.7\\.0\\(([0-9\\.]+).*\\)", current_data, re.M)
+            if m:
+                sys.stdout.write("Power: {} W\r\n".format(float(m.group(1)) * 1000))
+            current_data = ""
